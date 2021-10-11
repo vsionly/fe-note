c1b7dafcf7d6c8c315679f5edd51b246ad406e53
liweisheng <liweisheng@zuoshouyisheng.cn>
2021-10-11 16:38:38 +0800

    u

--- 文档/面试.js
+++ 文档/面试.js
@@ -15,7 +15,7 @@ html、css的个人见解
 
 1、利用Object.freeze()提升性能
     当你把一个普通的 JavaScript 对象传给 Vue 实例的  data  选项，Vue 将遍历此对象所有的属性，并使用  Object.defineProperty  把这些属性全部转为 getter/setter，这些 getter/setter 对用户来说是不可见的，但是在内部它们让 Vue 追踪依赖，在属性被访问和修改时通知变化。
-    但 Vue 在遇到像 Object.freeze() 这样被设置为不可配置之后的对象属性时，不会为对象加上 setter getter 等数据劫持的方法。参考 Vue 源码
+    但 Vue 在遇到像 Object.freeze() 这样被设置为不可配置之后的对象属性时，不会为对象加上 setter getter 等数据劫持的方法。参考Vue源码(vue会检测对象键的prop.configurable false时 不设置双向绑定)
 
 2、锁定对象的方法
 
@@ -33,8 +33,12 @@ html、css的个人见解
 
     same as seal, plus prevent existing properties and methods from being modified
     在上面的基础上，对象所有属性只读, 不可修改
-    以上三个方法分别可用Object.isExtensible(), Object.isSealed(), Object.isFrozen()来检测
+    以上三个方法分别可用Object.isExtensible(), Object.isSealed(), Object.isFrozen()来检测，都可以设置configurable为false
 
+    function getConfigurable (o, k) {
+        var prop = Object.getOwnPropertyDescriptor(o, k)
+        return prop && prop.configurable
+    }
 ******************************************************************************
 
 ******************************************************************************
